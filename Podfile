6429f0b543f5ac762102dc5e9acb8866b7d69f74
ECE Tech <ecetech@E3-C317-IMAC04.local>
2018-03-16 12:26:34 +0100

    1er commit

---
+++ Podfile
@@ -0,0 +1,6 @@
+# platform :ios, '9.0'
+ 
+target 'Sitter' do
+ use_frameworks!
+ pod 'Alamofire', '~> 4.3'
+end
---
+++ Sitter/WelcomeViewController.swift
@@ -0,0 +1,24 @@
+//
+//  WelcomeViewController.swift
+//  Sitter
+//
+//  Created by ECE Tech on 16/03/2018.
+//  Copyright © 2018 ECE Tech. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
